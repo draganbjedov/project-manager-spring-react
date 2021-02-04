979bbf374d2a7244eee454fd3b5a2f9133d0a80d
Dragan Bjedov <draganbjedov@gmail.com>
2021-02-04 19:38:59 +0100

    Initial commit

---
+++ project-manager-spring/src/test/java/org/bitbucket/draganbjedov/project/manager/ProjectManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bitbucket.draganbjedov.project.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
