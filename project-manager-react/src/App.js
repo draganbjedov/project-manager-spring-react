2ad4843a3381e6ea635553ae9d96093d1e03277f
Dragan Bjedov <draganbjedov@gmail.com>
2021-02-06 20:34:13 +0100

    Route to Create project

--- project-manager-react/src/App.js
+++ project-manager-react/src/App.js
@@ -1,13 +1,21 @@
 import "./App.css";
+import Header from "./components/layout/Header";
 import Dashboard from "./components/Dashboard";
+import CreateEditProject from "./components/project/CreateEditProject";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <Dashboard />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/create-project" component={CreateEditProject} />
+      </div>
+    </Router>
   );
 }
 
--- project-manager-react/src/components/Dashboard.js
+++ project-manager-react/src/components/Dashboard.js
@@ -1,20 +1,17 @@
 import React, { Component } from "react";
-import Header from "./layout/Header";
+import CreateProjectButton from "./project/CreateProjectButton";
 import Project from "./project/Project";
 
 class Dashboard extends Component {
   render() {
     return (
       <div className="projects">
-        <Header />
         <div className="container">
           <div className="row">
             <div className="col-md-12">
               <h1 className="display-4 text-center">Projects</h1>
               <br />
-              <a href="ProjectForm.html" className="btn btn-lg btn-info">
-                Create a Project
-              </a>
+              <CreateProjectButton />
               <br />
               <hr />
               <Project />
