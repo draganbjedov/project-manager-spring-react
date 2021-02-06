730f29174e7272c29c39e2b1064e5f80d1be63d4
Dragan Bjedov <draganbjedov@gmail.com>
2021-02-06 22:44:26 +0100

    Defined Redux store

--- project-manager-react/src/App.js
+++ project-manager-react/src/App.js
@@ -2,20 +2,25 @@ import "./App.css";
 import Header from "./components/layout/Header";
 import Dashboard from "./components/Dashboard";
 import CreateEditProject from "./components/project/CreateEditProject";
+import store from "./store";
+
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Route path="/dashboard" component={Dashboard} />
-        <Route path="/create-project" component={CreateEditProject} />
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Header />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/create-project" component={CreateEditProject} />
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
