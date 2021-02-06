2a329ac4e3dade863a3acf52072d1cd5285b1afe
Dragan Bjedov <draganbjedov@gmail.com>
2021-02-06 20:00:43 +0100

    Layout: Dashboard, Header and Project

--- project-manager-react/src/App.js
+++ project-manager-react/src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import Dashboard from "./components/Dashboard";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 function App() {
   return (
--- project-manager-react/src/components/Dashboard.js
+++ project-manager-react/src/components/Dashboard.js
@@ -1,10 +1,26 @@
 import React, { Component } from "react";
+import Header from "./layout/Header";
+import Project from "./project/Project";
 
 class Dashboard extends Component {
   render() {
     return (
-      <div>
-        <h1>Welcome to the Dashboard</h1>
+      <div className="projects">
+        <Header />
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4 text-center">Projects</h1>
+              <br />
+              <a href="ProjectForm.html" className="btn btn-lg btn-info">
+                Create a Project
+              </a>
+              <br />
+              <hr />
+              <Project />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
