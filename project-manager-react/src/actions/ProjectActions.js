4f452fd7595518f01e90ba155aaae355ed5989f1
Dragan Bjedov <draganbjedov@gmail.com>
2021-02-08 12:29:24 +0100

    Show list of projects in Dashboard

--- project-manager-react/src/actions/ProjectActions.js
+++ project-manager-react/src/actions/ProjectActions.js
@@ -1,9 +1,9 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS, GET_PROJECTS } from "./types";
 
 export const createProject = (project, history) => async (dispatch) => {
   try {
-    const res = await axios.post("http://localhost:8080/api/project", project);
+    await axios.post("http://localhost:8080/api/project", project);
     history.push("/dashboard");
   } catch (error) {
     dispatch({
@@ -11,4 +11,12 @@ export const createProject = (project, history) => async (dispatch) => {
       payload: error.response.data,
     });
   }
+};
+
+export const getProjects = () => async (dispatch) => {
+  const response = await axios.get("http://localhost:8080/api/project");
+  dispatch({
+    type: GET_PROJECTS,
+    payload: response.data,
+  });
 };
--- project-manager-react/src/components/Dashboard.js
+++ project-manager-react/src/components/Dashboard.js
@@ -2,8 +2,17 @@ import React, { Component } from "react";
 import CreateProjectButton from "./project/CreateProjectButton";
 import Project from "./project/Project";
 
+import { PropTypes } from "prop-types";
+import { getProjects } from "../actions/ProjectActions";
+import { connect } from "react-redux";
+
 class Dashboard extends Component {
+  componentDidMount() {
+    this.props.getProjects();
+  }
+
   render() {
+    const { projects } = this.props;
     return (
       <div className="projects">
         <div className="container">
@@ -14,7 +23,9 @@ class Dashboard extends Component {
               <CreateProjectButton />
               <br />
               <hr />
-              <Project />
+              {projects.map((project) => (
+                <Project key={project.id} project={project} />
+              ))}
             </div>
           </div>
         </div>
@@ -23,4 +34,14 @@ class Dashboard extends Component {
   }
 }
 
-export default Dashboard;
+Dashboard.propTypes = {
+  getProjects: PropTypes.func.isRequired,
+  projects: PropTypes.array.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  // state.project = because from src/reducers/index.js 'project' is name of reducer object
+  projects: state.project.projects,
+});
+
+export default connect(mapStateToProps, { getProjects })(Dashboard);
