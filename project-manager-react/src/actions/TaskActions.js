247a7da5f293ffe1247de0244119e6a0cbe498b8
Dragan Bjedov <draganbjedov@gmail.com>
2021-02-10 22:00:13 +0100

    Task actions and reducer

---
+++ project-manager-react/src/actions/TaskActions.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+import { GET_ERRORS, GET_TASKS, GET_TASK, DELETE_TASK } from "./types";
+
+export const createTask = (task, history) => async (dispatch) => {
+  try {
+    await axios.post("/api/backlog", task);
+    history.push(`/board/${task.projectIdentifier}`);
+    // Clear errors
+    dispatch({
+      type: GET_ERRORS,
+      payload: {},
+    });
+  } catch (error) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: error.response.data,
+    });
+  }
+};
+
+export const getTasks = (projectIdentifier) => async (dispatch) => {
+  const response = await axios.get(`/api/backlog/${projectIdentifier}`);
+  dispatch({
+    type: GET_PROJECTS,
+    payload: response.data,
+  });
+};
+
+export const getTask = (projectIdentifier, projectSequence, history) => async (dispatch) => {
+  try {
+    const response = await axios.get(`/api/backlog/${projectIdentifier}/${projectSequence}`);
+    dispatch({
+      type: GET_PROJECT,
+      payload: response.data,
+    });
+  } catch (error) {
+    history.push(`/board/${projectIdentifier}`);
+  }
+};
+
+export const deleteTask = (projectIdentifier, projectSequence) => async (dispatch) => {
+  if (window.confirm("Are sure that you want delete task?")) {
+    await axios.delete(`/api/backlog/${projectIdentifier}/${projectSequence}`);
+    dispatch({
+      type: DELETE_PROJECT,
+      payload: projectSequence,
+    });
+  }
+};
---
+++ project-manager-react/src/reducers/backlogReducer.js
@@ -0,0 +1,30 @@
+import { GET_TASKS, GET_TASK, DELETE_TASK } from "../actions/types";
+
+const initialState = {
+  tasks: [],
+  task: {},
+};
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case GET_TASKS:
+      return {
+        ...state,
+        tasks: action.payload,
+      };
+    case GET_TASK:
+      return {
+        ...state,
+        task: action.payload,
+      };
+    case DELETE_TASK:
+      return {
+        ...state,
+        tasks: state.tasks.filter(
+          (task) => task.projectSequence !== action.payload
+        ),
+      };
+    default:
+      return state;
+  }
+}
